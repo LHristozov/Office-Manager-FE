e36fd65166081e404d8d67d77c4ae04dff0296cf
Lyubomir <Hristozov.Lyubomir@adesso.bg>
2018-11-12 14:15:48 +0200

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: ContentComponent,
+      children: [
+          // { path: 'events', component: EventsComponent },
+          // { path: 'destinations', component: DestinationsComponent },
+          // { path: 'destinations/new-destination', component: NewDestinationComponent },
+      ]
+  },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '/index' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
