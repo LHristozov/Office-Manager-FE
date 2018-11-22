39a68d3f2b384eba43453ed5da6542faa3094257
dimov <tihomir.dimov@adesso.bg>
2018-11-22 12:45:31 +0200

    ngx-loading

--- src/app/form-pop-up/form-pop-up.component.ts
+++ src/app/form-pop-up/form-pop-up.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder} from '@angular/forms';
 import { ItemService } from '../shared/items/item.service';
 import { Item } from '../shared/items/item';
 import { _MatListItemMixinBase } from '@angular/material';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-form-pop-up',
@@ -15,7 +16,9 @@ export class FormPopUpComponent implements OnInit {
   @Input()id: number;
   myForm: FormGroup;
   requestedItem: Item = new Item();
-  constructor(private itemService: ItemService, public activeModal: NgbActiveModal, private formBuilder: FormBuilder) {
+  public loading = false;
+  constructor(private itemService: ItemService, public activeModal: NgbActiveModal, private formBuilder: FormBuilder,
+    private toastr: ToastrService) {
     this.createForm();
   }
 
@@ -30,6 +33,7 @@ export class FormPopUpComponent implements OnInit {
     });
   }
   private submitForm() {
+    this.loading = true;
     this.requestedItem.name = this.myForm.value.title;
     this.requestedItem.description = this.myForm.value.description;
     this.requestedItem.category = this.myForm.value.category;
@@ -37,9 +41,12 @@ export class FormPopUpComponent implements OnInit {
     this.requestedItem.isRequested = true;
 
     this.itemService.requestItem(this.requestedItem).then(res => {
+      this.loading = false;
+      this.toastr.success('Request product send!');
       this.activeModal.close('PopUp Closed');
     }).catch(err => {
-      console.error(err);
+      this.loading = false;
+      this.toastr.error(err.message);
     });
   }
 }
